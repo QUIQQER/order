9bb79032359c7ed9622f09fc435d33ce073d5a95
Henning Leutz <leutz@pcsg.de>
2018-10-08 13:55:40 +0200

    feat: quiqqer/order#53

---
+++ src/QUI/ERP/Order/Cron/CleanupOrderInProcess.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * This file contains QUI\ERP\Order\Cron\CleanupOrderInProcess
+ */
+
+namespace QUI\ERP\Order\Cron;
+
+use QUI;
+use QUI\ERP\Order\Handler;
+
+/**
+ * Class CleanupOrderInProcess
+ *
+ * This cron cleans old orders that have not been executed and are no longer needed.
+ * This cron does not influence any orders but only orders in process.
+ * Orders in process are canceled orders.
+ *
+ * @package QUI\ERP\Order\Crons
+ */
+class CleanupOrderInProcess
+{
+    /**
+     * Execute the cron
+     *
+     * @param array $params - cron parameter
+     */
+    public static function run($params = [])
+    {
+        $days = 14; // 14 days
+
+        if (!is_array($params)) {
+            $params = [];
+        }
+
+        if (!empty($params['days'])) {
+            $days = (int)$params['days'];
+        }
+
+        $days   = $days * -1;
+        $c_date = strtotime($days.' day');
+        $c_date = date('Y-m-d H:i:s', $c_date);
+
+        $Handler = QUI\ERP\Order\Handler::getInstance();
+        $table   = $Handler->tableOrderProcess();
+
+        $orders = QUI::getDataBase()->fetch([
+            'from'  => $table,
+            'where' => [
+                'c_date' => [
+                    'value' => $c_date,
+                    'type'  => '<='
+                ]
+            ]
+        ]);
+
+        foreach ($orders as $order) {
+            QUI::getDataBase()->delete($table, [
+                'id' => $order['id']
+            ]);
+        }
+    }
+}
