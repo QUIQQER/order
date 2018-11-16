acf5cd97b9ada59cc1cfbe064e4217c05d86180d
Henning Leutz <leutz@pcsg.de>
2018-11-16 11:44:06 +0100

    fix: basket not found -> frontend -> create new basket

--- ajax/frontend/basket/getBasket.php
+++ ajax/frontend/basket/getBasket.php
@@ -5,6 +5,7 @@
  */
 
 use QUI\ERP\Order\Handler as OrderHandler;
+use QUI\ERP\Order\Factory as OrderFactory;
 
 /**
  * Return the basket from the user
@@ -14,9 +15,13 @@ use QUI\ERP\Order\Handler as OrderHandler;
 QUI::$Ajax->registerFunction(
     'package_quiqqer_order_ajax_frontend_basket_getBasket',
     function () {
-        return OrderHandler::getInstance()->getBasketFromUser(
-            QUI::getUserBySession()
-        )->toArray();
+        try {
+            $Basket = OrderHandler::getInstance()->getBasketFromUser(QUI::getUserBySession());
+        } catch (QUI\Exception $Exception) {
+            $Basket = OrderFactory::getInstance()->createBasket(QUI::getUserBySession());
+        }
+
+        return $Basket->toArray();
     },
     false
 );
