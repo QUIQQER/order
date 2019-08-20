03a408be3b48c6e565725d09d91c1cd71b207d01
michael.daniel <michael@pcsg.de>
2019-08-20 16:37:09 +0200

    refactor: Code style.

--- types/shoppingCart.php
+++ types/shoppingCart.php
@@ -10,27 +10,18 @@ try {
 
     $User = QUI::getUserBySession();
 
-    QUI\System\Log::writeRecursive('1111111111111');
     // guest
     if (!QUI::getUsers()->isUser($User) || $User->getType() == QUI\Users\Nobody::class) {
-        QUI\System\Log::writeRecursive('222222222');
-
         $Registration = new Controls\Checkout\Registration();
         $Login        = new Controls\Checkout\Login();
-        QUI\System\Log::writeRecursive('22222222-------------------');
-
     } else {
-        QUI\System\Log::writeRecursive('333333333333333');
-
         // user is logged
         $Orders = QUI\ERP\Order\Handler::getInstance();
         $Basket = $Orders->getBasketFromUser($User);
-        QUI\System\Log::writeRecursive('44444444444');
 
         $BasketControl = new QUI\ERP\Order\Controls\OrderProcess\Basket([
             'basketId' => $Basket->getId()
         ]);
-        QUI\System\Log::writeRecursive('55555555555555');
 
         try {
             $OrderProcessSite = QUI\ERP\Order\Utils\Utils::getOrderProcess($Project);
@@ -40,7 +31,6 @@ try {
 
         $checkoutUrl = $OrderProcessSite->getUrlRewritten();
     }
-    QUI\System\Log::writeRecursive('aaaaaaaaaaaaaa');
 
     $Engine->assign([
         'Basket'       => $BasketControl,
@@ -48,9 +38,6 @@ try {
         'Registration' => $Registration,
         'Login'        => $Login
     ]);
-
-    QUI\System\Log::writeRecursive('bbbbbbbbbbbbbbb');
-
 } catch (QUI\DataBase\Exception $Exception) {
     $ExceptionReplacement = new QUI\Exception(['quiqqer/quiqqer', 'exception.error']);
 
