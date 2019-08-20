6cb796ef5dd41feacd76f47cb53954213f0e05c9
michael.daniel <michael@pcsg.de>
2019-08-20 16:34:52 +0200

    fix: Shopping cart site type: login and register if user is not logged in.

--- types/shoppingCart.php
+++ types/shoppingCart.php
@@ -1,31 +1,56 @@
 <?php
 
+use QUI\ERP\Order\Controls;
+
 try {
+    $BasketControl = null;
+    $checkoutUrl   = false;
+    $Registration  = null;
+    $Login         = null;
+
     $User = QUI::getUserBySession();
 
+    QUI\System\Log::writeRecursive('1111111111111');
+    // guest
     if (!QUI::getUsers()->isUser($User) || $User->getType() == QUI\Users\Nobody::class) {
-        // @todo guest ordering
-        \header('Location: '.$Project->firstChild()->getUrlRewritten(), false, 301);
-        exit;
+        QUI\System\Log::writeRecursive('222222222');
+
+        $Registration = new Controls\Checkout\Registration();
+        $Login        = new Controls\Checkout\Login();
+        QUI\System\Log::writeRecursive('22222222-------------------');
+
+    } else {
+        QUI\System\Log::writeRecursive('333333333333333');
+
+        // user is logged
+        $Orders = QUI\ERP\Order\Handler::getInstance();
+        $Basket = $Orders->getBasketFromUser($User);
+        QUI\System\Log::writeRecursive('44444444444');
+
+        $BasketControl = new QUI\ERP\Order\Controls\OrderProcess\Basket([
+            'basketId' => $Basket->getId()
+        ]);
+        QUI\System\Log::writeRecursive('55555555555555');
+
+        try {
+            $OrderProcessSite = QUI\ERP\Order\Utils\Utils::getOrderProcess($Project);
+        } catch (QUI\Exception $Exception) {
+            $OrderProcessSite = $Project->firstChild();
+        }
+
+        $checkoutUrl = $OrderProcessSite->getUrlRewritten();
     }
-
-    $Orders = QUI\ERP\Order\Handler::getInstance();
-    $Basket = $Orders->getBasketFromUser($User);
-
-    $BasketControl = new QUI\ERP\Order\Controls\OrderProcess\Basket([
-        'basketId' => $Basket->getId()
-    ]);
-
-    try {
-        $OrderProcessSite = QUI\ERP\Order\Utils\Utils::getOrderProcess($Project);
-    } catch (QUI\Exception $Exception) {
-        $OrderProcessSite = $Project->firstChild();
-    }
+    QUI\System\Log::writeRecursive('aaaaaaaaaaaaaa');
 
     $Engine->assign([
-        'Basket'      => $BasketControl,
-        'checkoutUrl' => $OrderProcessSite->getUrlRewritten(),
+        'Basket'       => $BasketControl,
+        'checkoutUrl'  => $checkoutUrl,
+        'Registration' => $Registration,
+        'Login'        => $Login
     ]);
+
+    QUI\System\Log::writeRecursive('bbbbbbbbbbbbbbb');
+
 } catch (QUI\DataBase\Exception $Exception) {
     $ExceptionReplacement = new QUI\Exception(['quiqqer/quiqqer', 'exception.error']);
 
