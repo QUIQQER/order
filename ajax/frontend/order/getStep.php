32c47bd1100ceaafdfb2e81630d704eddbbf3edd
Henning Leutz <leutz@pcsg.de>
2019-11-07 15:29:38 +0100

    fix: better exception handling

--- ajax/frontend/order/getStep.php
+++ ajax/frontend/order/getStep.php
@@ -14,35 +14,41 @@
 QUI::$Ajax->registerFunction(
     'package_quiqqer_order_ajax_frontend_order_getStep',
     function ($orderId, $step, $orderHash, $basketEditable) {
-        if (!isset($basketEditable) || $basketEditable === '') {
-            $basketEditable = true;
+        try {
+            if (!isset($basketEditable) || $basketEditable === '') {
+                $basketEditable = true;
+            }
+
+            $_REQUEST['current'] = $step;
+
+            $OrderProcess = new QUI\ERP\Order\OrderProcess([
+                'orderId'        => (int)$orderId,
+                'orderHash'      => $orderHash,
+                'basketEditable' => \boolval($basketEditable)
+            ]);
+
+            $Current = $OrderProcess->getCurrentStep();
+
+            if (!$Current) {
+                $Current = $OrderProcess->getFirstStep();
+            }
+
+            $OrderProcess->setAttribute('step', $Current->getName());
+
+            $html    = $OrderProcess->create();
+            $current = $OrderProcess->getCurrentStep()->getName();
+
+            return [
+                'html' => $html,
+                'step' => $current,
+                'url'  => $OrderProcess->getStepUrl($Current->getName()),
+                'hash' => $OrderProcess->getStepHash()
+            ];
+        } catch (\Exception $Exception) {
+            QUI\System\Log::writeException($Exception);
+
+            throw new QUI\Exception('Something went wrong');
         }
-
-        $_REQUEST['current'] = $step;
-
-        $OrderProcess = new QUI\ERP\Order\OrderProcess([
-            'orderId'        => (int)$orderId,
-            'orderHash'      => $orderHash,
-            'basketEditable' => \boolval($basketEditable)
-        ]);
-
-        $Current = $OrderProcess->getCurrentStep();
-
-        if (!$Current) {
-            $Current = $OrderProcess->getFirstStep();
-        }
-
-        $OrderProcess->setAttribute('step', $Current->getName());
-
-        $html    = $OrderProcess->create();
-        $current = $OrderProcess->getCurrentStep()->getName();
-
-        return [
-            'html' => $html,
-            'step' => $current,
-            'url'  => $OrderProcess->getStepUrl($Current->getName()),
-            'hash' => $OrderProcess->getStepHash()
-        ];
     },
     ['orderId', 'step', 'orderHash', 'basketEditable']
 );
